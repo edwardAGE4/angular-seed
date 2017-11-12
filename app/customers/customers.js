a08167be0fc9a428e94c26540671134a675365c6
AYANOU Giovanni <ayanougiovanni@gmail.com>
2017-11-12 23:58:36 +0100

    Modification Détail et suppression

--- app/customers/customers.js
+++ app/customers/customers.js
@@ -9,37 +9,91 @@ angular.module('myApp.customers', ['ngRoute'])
             controller: 'CustomersCtrl'
         })
     
-        .when('/customers/Add', {
-            templateUrl: 'customers/customersAdd.html',
-            controller: 'CustomersAdd'
+        .when('/customers/Action', {
+            templateUrl: 'customers/customersAction.html',
+            controller: 'CustomersAction'
         })
+    
+        .when('/customers/Details', {
+            templateUrl: 'customers/customersDetails.html',
+            controller: 'CustomersDetails'
+        })
+    
 }])
 
 
 
 //controleur de customers
 
-.controller('CustomersCtrl', ['$scope','customersProvider', function($scope, customersProvider) {
+.controller('CustomersCtrl', ['$rootScope','$scope','customersProvider','$location', function($rootScope,$scope, customersProvider,$location) {
     
+    //Initialisation des valeurs 
     $scope.customers = customersProvider.initCustomer();
     
-}])
-
-.controller('CustomersAdd', ['$scope','customersProvider', function($scope, customersProvider) {
-    
-    $scope.newcustomer = function (customer){
-        $scope.customers = customersProvider.new(customer);
-        console.log($scope.customers);
+    //Méthode de suppression
+     $scope.rmdcustomer = function (customer){
+        // console.log(customer);
+        $scope.customers = customersProvider.rmd(customer);
+    }
+     
+    //Choix d'action : Modification
+        $scope.updcustomer = function (customer){
+        $rootScope.New_Customer = customer;
+        $rootScope.Action = 1; //si l'action est 0 c'est une insertion et si Action est 1 c'est une Modification
+        $location.path('/customers/Action')
+    }
+        
+     //Choix d'action : Ajout
+        $scope.addcustomer = function (){
+        $rootScope.New_Customer = customersProvider.newcust() ;
+        $rootScope.Action = 0; //si l'action est 0 c'est une insertion et si Action est 1 c'est une Modification
+        $location.path('/customers/Action')
+    }
+        
+     //Choix d'action : Détails
+        $scope.dtlcustomer = function (customer){
+        $rootScope.New_Customer = customer ; 
+        $location.path('/customers/Details')
     }
     
 }])
 
-.controller('CustomersRmd', ['$scope', function($scope) {
+.controller('CustomersAction', ['$rootScope','$scope','customersProvider','$location', function($rootScope,$scope, customersProvider, $location) {
     
+    $scope.newcustomer = function (){
+        // si on a une insertion 
+        
+        if($rootScope.Action == 0)  
+            {
+                //console.log($rootScope.New_Customer);
+                $rootScope.New_Customer.index = customersProvider.last_index();
+                $scope.customers = customersProvider.new($rootScope.New_Customer);
+            }
+        //dans le cas d'une modification    
+        else  $scope.customers = customersProvider.update($rootScope.New_Customer);
+        
+        console.log($scope.customers);
+        $location.path('/customers')
+    }
     
 }])
 
-.controller('CustomersUpd', ['$scope', function($scope) {
+.controller('CustomersDetails', ['$rootScope','$scope','customersProvider','$location', function($rootScope,$scope, customersProvider, $location) {
     
+   /* $scope.newcustomer = function (){
+        // si on a une insertion 
+        
+        if($rootScope.Action == 0)  
+            {
+                //console.log($rootScope.New_Customer);
+                $rootScope.New_Customer.index = customersProvider.last_index();
+                $scope.customers = customersProvider.new($rootScope.New_Customer);
+            }
+        //dans le cas d'une modification    
+        else  $scope.customers = customersProvider.update($rootScope.New_Customer);
+        
+        console.log($scope.customers);
+        $location.path('/customers')
+    }*/
     
-}]);
+}])
--- app/customers/customersProvider.js
+++ app/customers/customersProvider.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-var customer = [
+var liste_customer = [
   {
     "_id": "553634f5cc69d84bc3c46ef8",
     "index": 0,
@@ -74,12 +74,65 @@ var customer = [
 
 app.service('customersProvider',function(){
   
+    //Initialisation de la liste de customers
     this.initCustomer=function(){
-       return customer;
+       return liste_customer;
    }
     
+    //création d'un nouvel objet customer
+    this.newcust = function(){
+        var cust =   {
+                "_id": "",
+                "index": 0,
+                "age": 0,
+                "eyeColor": "",
+                "name": "",
+                "gender": "",
+                "company": "",
+                "email": "",
+                "phone": "",
+                "address": "",
+                "about": ""
+            }
+        return cust;
+    }
+    
+    //Ajout d'un nouveau customer
     this.new=function(Newcustomer){
-         customer.push(Newcustomer);
-         return customer;
+         liste_customer.push(Newcustomer);
+         return liste_customer;
+   }
+    
+    //Modification
+    this.update=function(Newcustomer){
+         //liste_customer.push(Newcustomer);
+         liste_customer.forEach(function(customer) {
+           if(customer.index == Newcustomer.index){customer = Newcustomer};
+         });
+         return liste_customer;
+   }
+    
+    //suppression de customers
+     this.rmd=function(Newcustomer){
+         var i=0,j=0;
+         //Parcour de la liste pour rcupérer l'index
+         liste_customer.forEach(function(customer) {
+            
+              if(customer.index == Newcustomer.index){j=i;}
+             i++;
+         });
+         
+         //suppression
+         liste_customer.splice(j,1);
+         return liste_customer;
+   }
+    
+    //auto incrément d'index
+    this.last_index=function(){
+         var Vindex = -1;
+         liste_customer.forEach(function(customer) {
+              Vindex = customer.index;
+         });
+         return Vindex +1;
    }
 });
